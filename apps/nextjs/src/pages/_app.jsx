213f08c413076335888b317734ba65461ae5fb65
“ayocodess” <ayo.daniel.adesnya@gmail.com>
2023-12-20 04:29:50 +0100

    update

--- apps/nextjs/src/pages/_app.jsx
+++ apps/nextjs/src/pages/_app.jsx
@@ -1,18 +1,15 @@
 // src/pages/_app.tsx
 import "../styles/globals.css";
-import type { AppType } from "next/app";
+// import type { AppType } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
 import { trpc } from "../utils/trpc";
 
-const MyApp: AppType = ({ Component,pageProps: { ...pageProps } }) => {
+const MyApp = ({ Component, pageProps }) => {
   return (
-
     <ClerkProvider {...pageProps}>
-      {(Component as React.ComponentType<unknown>)(pageProps)}
+      <Component {...pageProps} />
     </ClerkProvider>
   );
 };
-
-
 
 export default trpc.withTRPC(MyApp);
